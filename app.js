6a332d681f14726da0445f2dadb5e463da43fdc5
Siddharath Singh <siddharathsingh19@gmail.com>
2024-03-07 20:53:01 +0530

    Removed bug from AM PM func

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ function currentTime(){
     let hours = now.getHours();
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
-    const ampm = hours >= 12 ? "AM" : "PM";
     hours = hours % 12 || 12 ;
+    const ampm = hours >= 12 ? "AM" : "PM";
 
     document.getElementById('hours').textContent = hours.toString().padStart(2,'0') + " :";
     document.getElementById('minutes').textContent = minutes + " :";
